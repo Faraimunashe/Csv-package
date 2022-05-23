852aca46eef0314dc25cce7fb0c8481938ea06b5
unknown <faraimunshe.m11@gmail.com>
2022-05-24 00:02:44 +0200

    Added error array

--- src/CheckFile.php
+++ src/CheckFile.php
@@ -4,6 +4,7 @@ namespace Faraimunashe\Csv;
 
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CheckFile
 {
@@ -15,6 +16,7 @@ class CheckFile
         $this->result = (object)[
             "extension" => null,
             "headers" => null,
+            "error" => null,
             "exception" => null
         ];
     }
@@ -76,6 +78,7 @@ class CheckFile
         $headers = fgetcsv($csv, 0, $this->delimiter);
 
         $rows = array();
+        $error = array();
         $row_number = 0;
         while ($csv_row = fgetcsv($csv, 0, $this->delimiter)) {
 
@@ -87,15 +90,7 @@ class CheckFile
                 return 'Row ' . $row_number . '\'s length does not match the header length: ' . implode(', ', $encoded_row);
             } else {
                 if ($encoded_row[0] === "" || $encoded_row[1] === "" || $encoded_row[2] === "" || $encoded_row[3] === "" || $encoded_row[4] === "" || $encoded_row[5] === "" || $encoded_row[6] === "" || $encoded_row[7] === "" || $encoded_row[8] === "" || $encoded_row[9] === "" || $encoded_row[10] === "") {
-                    $error = [
-                        "message" => "There is a null cell on row: " . $row_number
-                    ];
-                    return $this->result = (object)[
-                        "extension" => $extension,
-                        "headers" => (object)$headers,
-                        "rows" => null,
-                        "exception" => (object)$error
-                    ];
+                    $error[] = array_combine(["message" => "There is a null cell on row: "],  ["row_umber" => $row_number]);
                 } else {
                     $rows[] = array_combine($headers, $encoded_row);
                 }
@@ -103,15 +98,31 @@ class CheckFile
 
             //if( $row_number === 5 ) break;
         }
-
+        if (!empty($error)) {
+            return $this->result = (object)[
+                "extension" => $extension,
+                "headers" => (object)$headers,
+                "rows" => null,
+                "error" => (object)$error,
+                "exception" => null
+            ];
+        }
 
         if (!is_null($rows)) {
             return $this->result = (object)[
                 "extension" => $extension,
                 "headers" => (object)$headers,
-                "rows" => (object)$rows,
+                "rows" => $rows,
+                "error" => null,
                 "exception" => null
             ];
         }
+    }
+
+    public function validation(array $rows, array $rules)
+    {
+        $validator = Validator::make($rows, $rules);
+
+        return $validator;
     }
 }
